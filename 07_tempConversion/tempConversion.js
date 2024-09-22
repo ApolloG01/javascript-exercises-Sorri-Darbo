9f210a14ed33cc03ec60fead272de2ad6a2facc3
“Sorri” <37029743+ApolloG01@users.noreply.github.com>
2024-09-22 18:38:49 +0200

    completed exercise 7 Tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  console.log((temp - 32) * (5 / 9));
+  const celsius = (temp - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  console.log(temp * (9 / 5) + 32);
+  const farenheight = temp * (9 / 5) + 32;
+  return Number(farenheight.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
