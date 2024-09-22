4275e580e293cbba28f7c6a02de843159831c937
“Sorri” <37029743+ApolloG01@users.noreply.github.com>
2024-09-22 12:30:26 +0200

    completato esercizio 06 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (...arg) {
+  let sum = 0;
 
+  if (arg[0] < arg[1]) {
+    for (let i = arg[0]; i <= arg[1]; i++) {
+      sum = sum + i;
+    }
+
+    if (
+      typeof arg[1] === "number" &&
+      arg[0] > 0 &&
+      arg[1] > 0 &&
+      Number.isInteger(arg[0]) &&
+      Number.isInteger(arg[1]) &&
+      typeof arg[1] === "number"
+    ) {
+      console.log(typeof [arg[1]]);
+      return sum;
+    } else {
+      return "ERROR";
+    }
+  } else if (arg[0] > arg[1]) {
+    for (let i = arg[1]; i <= arg[0]; i++) {
+      sum = sum + i;
+    }
+    console.log(sum);
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
